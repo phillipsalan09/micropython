217966663cb178add84ba5959d0303da4de59410
phillipsalan09 <phillipsalan09@googlemail.com>
2026-09-23 22:06:45 +0100

    Update mpconfigboard.cmake
    ammended to auto memory detection

--- ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.cmake
+++ ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.cmake
@@ -1,6 +1,6 @@
 # cmake file for Raspberry Pi Pico
 set(PICO_BOARD "pico")
 set(PICO_PLATFORM "rp2040")
-if(NOT DEFINED MICROPY_HW_FLASH_STORAGE_BYTES)
-    set(MICROPY_HW_FLASH_STORAGE_BYTES 15728640)  # 15 * 1024 * 1024 (15MB for filesystem)
-endif()
+
+# Automatically calculate all remaining flash space for the filesystem
+set(MICROPY_HW_FLASH_STORAGE_BYTES AUTO)
