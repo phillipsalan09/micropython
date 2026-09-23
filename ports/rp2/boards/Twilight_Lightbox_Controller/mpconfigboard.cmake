bcf151a5067f2b224abab78343729e4d81b479f7
phillipsalan09 <phillipsalan09@googlemail.com>
2026-09-23 22:21:49 +0100

    Memory sizes update
    updated memory sizes

--- ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.cmake
+++ ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.cmake
@@ -1,6 +1,9 @@
-# cmake file for Raspberry Pi Pico
+# cmake file for Twilight Lightbox Controller
 set(PICO_BOARD "pico")
 set(PICO_PLATFORM "rp2040")
 
-# Automatically calculate all remaining flash space for the filesystem
-set(MICROPY_HW_FLASH_STORAGE_BYTES AUTO)
+# Tell the Pico SDK hardware compiler that this chip has 16MB total capacity
+set(PICO_FLASH_SIZE_BYTES 16777216)
+
+# Allocate 14.5MB specifically for the user storage filesystem
+set(MICROPY_HW_FLASH_STORAGE_BYTES 15204352)
--- ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.h
+++ ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.h
@@ -1,5 +1,4 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME          "Twilight Controller V1"
 #define MICROPY_HW_FLASH_SIZE_BYTES    (16 * 1024 * 1024)
-
-// Force cloud build trigger
+#define MICROPY_HW_FLASH_STORAGE_BYTES (14848 * 1024)
