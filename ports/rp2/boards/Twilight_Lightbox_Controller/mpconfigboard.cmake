545f8f0eaad16256dfc68774a213a7eb0d9949e7
phillipsalan09 <phillipsalan09@googlemail.com>
2026-09-24 10:07:24 +0100

    memory syntax

--- ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.cmake
+++ ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.cmake
@@ -2,8 +2,8 @@
 set(PICO_BOARD "pico")
 set(PICO_PLATFORM "rp2040")
 
-# Tell the Pico SDK hardware compiler that this chip has 16MB total capacity
+# Tell the hardware engine the chip size is 16MB
 set(PICO_FLASH_SIZE_BYTES 16777216)
 
-# Allocate 14.5MB specifically for the user storage filesystem
-set(MICROPY_HW_FLASH_STORAGE_BYTES 15204352)
+# Tell the filesystem engine to map out 15MB
+set(MICROPY_HW_FLASH_STORAGE_BYTES 15728640)
--- ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.h
+++ ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.h
@@ -1,3 +1,4 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME          "Twilight Controller V1"
-#define MICROPY_HW_FLASH_STORAGE_BYTES (PICO_FLASH_SIZE_BYTES - (1024 * 1024))
+#define MICROPY_HW_FLASH_SIZE_BYTES    (16 * 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES (15 * 1024 * 1024)
