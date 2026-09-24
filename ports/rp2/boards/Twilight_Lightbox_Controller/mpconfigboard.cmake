8c351b69ba6fe80df687e11d720944f4af60a9f7
phillipsalan09 <phillipsalan09@googlemail.com>
2026-09-24 10:16:13 +0100

    cmake and config memory syntax

--- ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.cmake
+++ ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.cmake
@@ -2,8 +2,8 @@
 set(PICO_BOARD "pico")
 set(PICO_PLATFORM "rp2040")
 
-# Tell the hardware engine the chip size is 16MB
+# Tell the core compiler the hardware chip is 16MB
 set(PICO_FLASH_SIZE_BYTES 16777216)
 
-# Tell the filesystem engine to map out 15MB
+# Allocate exactly 15MB to the user file system
 set(MICROPY_HW_FLASH_STORAGE_BYTES 15728640)
--- ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.h
+++ ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.h
@@ -1,4 +1,4 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME          "Twilight Controller V1"
 #define MICROPY_HW_FLASH_SIZE_BYTES    (16 * 1024 * 1024)
-#define MICROPY_HW_FLASH_STORAGE_BYTES (15 * 1024 * 1024)
+
