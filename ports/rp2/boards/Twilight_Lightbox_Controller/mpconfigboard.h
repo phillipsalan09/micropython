bd14d23e8824861c4d7d1e7a3054caad1fa304c1
phillipsalan09 <phillipsalan09@googlemail.com>
2026-09-24 09:54:39 +0100

    Update mpconfigboard.h
    redefined memory code

--- ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.h
+++ ports/rp2/boards/Twilight_Lightbox_Controller/mpconfigboard.h
@@ -1,4 +1,3 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME          "Twilight Controller V1"
-#define MICROPY_HW_FLASH_SIZE_BYTES    (16 * 1024 * 1024)
-#define MICROPY_HW_FLASH_STORAGE_BYTES (14848 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES (PICO_FLASH_SIZE_BYTES - (1024 * 1024))
